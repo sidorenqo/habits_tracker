90b8d0e24165f58108bf4e577e0d95f6b8f25860
Gennady Sidorenko <sidorenqo@gmail.com>
2023-11-20 19:11:50 +0500

    Добавлены загрузка и сохранение данных

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,29 @@
 "use strict";
 
-const habbits = []
+let habbits = [];
+const HABBIT_KEY = "HABBIT_KEY";
+
+/**
+ * Получаем данные от пользователя
+ */
+
+function loadData() {
+  const habbitsString = localStorage.getItem(HABBIT_KEY); //Получаем строку
+  const habbitArray = JSON.parse(habbitsString); // Парсим стрингу в массив
+  // Делаем проверку что передаем массив
+  if (Array.isArray(habbitArray)) {
+    habbits = habbitArray;
+  }
+}
+
+/**
+ * Хранение данных
+ */
+
+function saveData() {
+  localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+(() => {
+  loadData();
+})();
