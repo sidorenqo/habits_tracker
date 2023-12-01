dedf29f27ea15d9ba73e66cd295f2c5d6fe3c75a
Gennady Sidorenko <sidorenqo@gmail.com>
2023-12-01 15:14:19 +0500

    Настроена форма добавления привычки

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@ const HABBIT_KEY = "HABBIT_KEY";
 let globalActiveHabbitId;
 
 const page = {
-  // Описание работы нашего объекта
+  // Описание работы нашего приложенния
   menu: document.querySelector(".menu__list"),
   header: {
     h1: document.querySelector(".h1"),
@@ -18,6 +18,7 @@ const page = {
   },
   popup: {
     index: document.getElementById("add-habbit_popup"),
+    iconField: document.querySelector('.popup__form input[name="icon"]'),
   },
 };
 
@@ -48,6 +49,35 @@ function togglePopup() {
   } else {
     page.popup.index.classList.add("cover_hidden");
   }
+}
+
+function resetForm(form, fields) {
+  for (const field of fields) {
+    form[field].value = "";
+  }
+}
+
+function validateAndGetFormData(form, fields) {
+  const formData = new FormData(form);
+  const res = {};
+  for (const field of fields) {
+    const fieldValue = formData.get(field);
+    form[field].classList.remove("error");
+    if (!fieldValue) {
+      form[field].classList.add("error");
+    }
+    res[field] = fieldValue;
+  }
+  let isValid = true;
+  for (const field of fields) {
+    if (!res[field]) {
+      isValid = false;
+    }
+  }
+  if (!isValid) {
+    return;
+  }
+  return res;
 }
 
 /**
@@ -121,25 +151,21 @@ function render(activeHabbitId) {
  */
 
 function addDays(event) {
-  const form = event.target;
   event.preventDefault();
-  console.log(event);
-  const data = new FormData(event.target);
-  const comment = data.get("comment");
-  form["comment"].classList.remove("error");
-  if (!comment) {
-    form["comment"].classList.add("error");
+  const data = validateAndGetFormData(event.target, ["comment"]);
+  if (!data) {
+    return;
   }
   habbits = habbits.map((habbit) => {
     if (habbit.id === globalActiveHabbitId) {
       return {
         ...habbit,
-        days: habbit.days.concat([{ comment }]),
+        days: habbit.days.concat([{ comment: data.comment }]),
       };
     }
     return habbit;
   });
-  form["comment"].value = "";
+  resetForm(event.target, ["comment"]);
   render(globalActiveHabbitId);
   saveData();
 }
@@ -157,6 +183,40 @@ function deleteDay(index) {
   });
   render(globalActiveHabbitId);
   saveData();
+}
+
+/**
+ * Работа с добавлением привычек
+ */
+
+function setIcon(context, icon) {
+  page.popup.iconField.value = icon;
+  const activeIcon = document.querySelector(".icon.icon_active");
+  activeIcon.classList.remove("icon_active");
+  context.classList.add("icon_active");
+}
+
+function addHabbit(event) {
+  event.preventDefault();
+  const data = validateAndGetFormData(event.target, ["name", "icon", "target"]);
+  if (!data) {
+    return;
+  }
+  const maxId = habbits.reduce(
+    (acc, habbit) => (acc > habbit.id ? acc : habbit.id),
+    0
+  );
+  habbits.push({
+    id: maxId + 1,
+    name: data.name,
+    target: data.target,
+    icon: data.icon,
+    days: [],
+  });
+  resetForm(event.target, ["name"], ["target"]);
+  togglePopup();
+  saveData();
+  render(maxId + 1);
 }
 
 /**
